e50b6052ab6eece7b74d16d2ae17444e7d47abad
Gustavo Hernandez <ghdzplus@gmail.com>
2024-02-22 11:12:07 -0600

    Component compound ready

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../assets/react.svg';
+import { ShoppingPage } from '../02-components-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
   return (
@@ -11,13 +12,13 @@ export const Navigation = () => {
           <img className="logo" src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink className={ ({ isActive }) => isActive ? "nav-active" : "" } to="/home">Home</NavLink>
+              <NavLink to="/home" className={ ({ isActive }) => isActive ? "nav-active" : "" } >Shopping</NavLink>
             </li>
             <li>
-              <NavLink className={ ({ isActive }) => isActive ? "nav-active" : "" } to="/about">About</NavLink>
+              <NavLink to="/about" className={ ({ isActive }) => isActive ? "nav-active" : "" } >About</NavLink>
             </li>
             <li>
-              <NavLink className={ ({ isActive }) => isActive ? "nav-active" : "" } to="/users">Users</NavLink>
+              <NavLink to="/users" className={ ({ isActive }) => isActive ? "nav-active" : "" } >Users</NavLink>
             </li>
           </ul>
         </nav>
@@ -25,9 +26,9 @@ export const Navigation = () => {
         <Routes>
           <Route path="about" element={ <h1>About Page</h1> } />
           <Route path="users" element={ <h1>Users Page</h1> } />
-          <Route path="home" element={ <h1>Home Page</h1> } />
+          <Route path="shopping" element={ <ShoppingPage /> } />
           
-          <Route path="/*" element={ <Navigate to="/home" replace /> } />
+          <Route path="/*" element={ <Navigate to="/shopping" replace /> } />
         </Routes>
       </div>
     </BrowserRouter>
