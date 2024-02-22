129feda183ba0291781d8ba98a2045f7ff6a57e4
Gustavo Hernandez <ghdzplus@gmail.com>
2024-02-22 13:12:20 -0600

    Component extend styles ready

--- src/02-components-patterns/components/ProductTitle.tsx
+++ src/02-components-patterns/components/ProductTitle.tsx
@@ -2,12 +2,21 @@ import { useContext } from 'react';
 import { ProductContext } from './ProductCard';
 import styles from '../styles/styles.module.css';
 
-export const ProductTitle = ({ title }: { title?: string }) => {
+export interface Props {
+    title?: string;
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+export const ProductTitle = ({ title, className, style }: Props) => {
 
     const { product } = useContext( ProductContext );
 
     return (
-        <div className={ styles.horizontalCenter }>
+        <div 
+            className={ `${ styles.horizontalCenter } ${ className }` }
+            style={ style }
+        >
             <span className={ styles.productDescription }>
                 { title ? title : product.title }
             </span>
